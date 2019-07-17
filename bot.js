19e639c254a7223944efd8fe73139297ceb72ef3
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2019-07-17 17:18:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1042,7 +1042,7 @@ client.on("message", async message => {
             client.on('message', message => {
                 if (message.content.startsWith('$Stockholm-info')){
                     const xHu = "16";
-                     let xHu = new Discord.RichEmbed()
+                     let xHu3 = new Discord.RichEmbed()
                      .setTitle("=== ( Stockholm Gang ) ===")
                      .addField('Owner Of The Gang :',"IGN : __ Martinez A.Stockholm __ | Discord : ")            
                      .addField(`Co.Owner OF The Gang : `, "IGN : __ Arther A.Stockholm __  | Discord : ! Zero 💔#6146")
@@ -1065,7 +1065,7 @@ client.on("message", async message => {
                      15- Hero A.Stockholm`)
                      .addField("Faction Name :",`__ xHu __`)
                   .setColor("RANDOM")
-                  message.channel.sendEmbed(xHu);
+                  message.channel.sendEmbed(xHu3);
                     }
                 });
 ////////////////////////////
